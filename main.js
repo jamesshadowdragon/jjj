ab5b61400d7d707118004d7002f11bdf882283fb
jamesshadowdragon <mhummadshhd@gmail.com>
2026-07-20 14:51:13 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1312,3 +1312,51 @@ loading.remove();
 
 
 });
+
+const revealObjects =
+document.querySelectorAll(
+".card,.glass,.skills div,.contactBox"
+);
+
+
+const observer =
+new IntersectionObserver(
+(entries)=>{
+
+entries.forEach(entry=>{
+
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity="1";
+
+entry.target.style.transform=
+"translateY(0)";
+
+}
+
+});
+
+
+},
+{
+threshold:.2
+}
+);
+
+
+
+revealObjects.forEach(el=>{
+
+el.style.opacity="0";
+
+el.style.transform=
+"translateY(50px)";
+
+el.style.transition=
+"1s";
+
+
+observer.observe(el);
+
+});
